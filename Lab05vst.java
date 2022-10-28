ad6d936eb01fca8b570c0dc9649d5b043dd5aaf9
Elise Giroux <elise.giroux@vtsd.com>
2022-10-28 10:24:44 -0400

    90 point version

--- Lab05vst.java
+++ Lab05vst.java
@@ -6,16 +6,43 @@ import java.awt.*;
 import java.applet.*;
 
 
-public class Lab05vst extends Applet
-{
-    public void paint(Graphics g)
-    {
+public class Lab05vst extends Applet {
+    public void paint(Graphics g) {
         int width = 980;
         int height = 630;
         int x1 = 10;
         int y1 = 640;
         int x2 = 990;
         int y2 = 640;
-        g.drawRect(10,10,width,height);
+        g.drawRect(10, 10, width, height);
+
+        //bottom right corner
+
+        x1 = 10;
+        y1 = 640;
+        x2 = 990;
+        y2 = 625;
+        for (int k = 0; k < 49; k++) {
+            g.drawLine(x1, y1, x2, y2);
+            x1 += 20;
+            y2 -= 12;
+
+            // top right corner
+
+
+        }
+        x1 = 10;
+        y1 = 640;
+        x2 = 990;
+        y2 = 640;
+
+        for (int k = 0; k < 49; k++) {
+            g.drawLine(x2, y2, x1, y1);
+            x2 -= 20;
+            y1 -= 13;
+
+
+        }
+
     }
 }
--- Temp.java
+++ Temp.java
@@ -10,7 +10,12 @@ public class Temp extends Applet
 {
     public void paint(Graphics g)
     {
+        int x=20;
+        int y=20;
+        int width= 30;
+        for(int k = 0; k < 50; k++){
 
+        }
 
 
     }
